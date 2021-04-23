2aca3997afbe3eab4779fd606a557b8ea0829c15
Yarik9001 <lynx9001@yandex.ru>
2021-04-23 17:38:09 +0300

    threead

---
+++ Experiment/threaded/therad-1.py
@@ -0,0 +1,32 @@
+from threading import Thread
+from time import sleep
+
+class Check():
+    def __init__(self):
+        self.name = 'Proteus0'
+        self.n = 0
+
+def one(a:Check, name):
+    while True:
+        sleep(0.001)
+        a.n += 1
+
+def two(a:Check, name):
+    while True:
+        sleep(0.2)
+        print(a.n)
+
+def thee(a:Check,name):
+    while True:
+        sleep(1)
+        a.n = 0
+
+
+
+ch = Check()
+a = Thread(target=one, name='one',args=(ch,'one'))
+b = Thread(target=two, name='tho',args=(ch, 'tho'))
+c = Thread(target=thee, name='three',args=(ch,'three'))
+a.start()
+b.start()
+c.start()
