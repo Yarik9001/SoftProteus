70ca7f7b2ec521bbe04cc7e5157d3060d865da9c
Yarik9001 <lynx9001@yandex.ru>
2021-06-02 09:51:15 +0300

    test-drk

--- Soft/UnitTests/Test-drk.py
+++ Soft/UnitTests/Test-drk.py
@@ -1,31 +1,59 @@
-import time
-from board import SCL, SDA
-import busio
-from adafruit_motor import servo
-from adafruit_pca9685 import PCA9685
+from time import sleep
+from adafruit_servokit import ServoKit
 
-PORT = None  # TODO - Определить порт к которому подключен шилд
-MINI = 900  # TODO - определить минимальную частоту 
-MAXI = 1900  # TODO - определить максимальную частоту 
-i2c = busio.I2C(SCL, SDA)
-
-# Create a simple PCA9685 class instance.
-pca = PCA9685(i2c, address=PORT)
-pca.frequency = 50
-
-servo7 = servo.Servo(pca.channels[7])
-servo7.set_pulse_width_range(MINI, MAXI)
-
-servo6 = servo.Servo(pca.channels[6])
-servo6.set_pulse_width_range(MINI, MAXI)
-
-def testOne():    
-    # test-one управление дрк как сервоприводом 
-    for i in range(180):
-        servo7.angle = i
-        time.sleep(0.03)
+# Set channels to the number of servo channels on your kit.
+# 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
+class DrkMotor:
+    def __init__(self):
+        self.kit = ServoKit(channels=16)
         
-    for i in range(180):
-        servo7.angle = 180 - i
-        time.sleep(0.03)
-
+        self.drk0 = self.kit.servo[0]
+        self.drk1 = self.kit.servo[1]
+        self.drk2 = self.kit.servo[2]
+        self.drk3 = self.kit.servo[3]
+        self.drk4 = self.kit.servo[4]
+        self.drk5 = self.kit.servo[5]
+        
+        print('init-motor')
+        
+    def initMotor(self):
+        self.drk0.angle = 180
+        self.drk1.angle = 180
+        self.drk2.angle = 180
+        self.drk3.angle = 180
+        self.drk4.angle = 180
+        self.drk5.angle = 180
+        print('motor max')
+        sleep(2)
+        self.drk0.angle = 0
+        self.drk1.angle = 0
+        self.drk2.angle = 0
+        self.drk3.angle = 0
+        self.drk4.angle = 0
+        self.drk5.angle = 0
+        print('motor min')
+        sleep(2)
+        self.drk0.angle = 90
+        self.drk1.angle = 90
+        self.drk2.angle = 90
+        self.drk3.angle = 90
+        self.drk4.angle = 90
+        self.drk5.angle = 90
+        print('motor center')
+        sleep(6)
+        print('motor good')
+        
+    
+        
+    
+        
+    # for i in range(len(kit.servo)):
+    #     kit.servo[i].angle = 180
+    # time.sleep(1)
+    # for i in range(len(kit.servo)):
+    #     kit.servo[i].angle = 0
+    # time.sleep(1)
+    
+if __name__ =='__main__': 
+    drk = DrkMotor()
+    drk.initMotor()
