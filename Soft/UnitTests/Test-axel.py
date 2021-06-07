faf185065ecdd01de71d09959585cabe9a1a90a7
Yarik9001 <lynx9001@yandex.ru>
2021-06-07 10:34:22 +0300

    axel

--- Soft/UnitTests/Test-axel.py
+++ Soft/UnitTests/Test-axel.py
@@ -1,8 +1,13 @@
-from mpu6050 import mpu6050
-from time import sleep 
+import time
+import board
+import adafruit_mpu6050
 
-sensor = mpu6050(0x68)
+i2c = board.I2C()  # uses board.SCL and board.SDA
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
 while True:
-    accelerometer_data = sensor.get_accel_data()
-    print(accelerometer_data)
-    sleep(0.25)
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+    print("Temperature: %.2f C" % mpu.temperature)
+    print("")
+    time.sleep(1)
