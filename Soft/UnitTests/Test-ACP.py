e0bea52f88cf7235505d72ed27e24b38f21edb49
Yarik9001 <lynx9001@yandex.ru>
2021-06-30 23:04:28 +0300

    ampermetr

--- Soft/UnitTests/Test-ACP.py
+++ Soft/UnitTests/Test-ACP.py
@@ -5,23 +5,52 @@ from adafruit_ads1x15.analog_in import AnalogIn
 from time import sleep
 ###################################################
 
-i2c = busio.I2C(board.SCL, board.SDA)
+class Acp:
+    def __init__(self):
+        '''
+        Класс описывающий взаимодействие и опрос датчиков тока 
+        '''
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.ads13 = ADS.ADS1115(self.i2c)
+        self.adc46 = ADS.ADS1115(self.i2c, address=0x49)
 
-ads13 = ADS.ADS1115(i2c)
-adc46 = ADS.ADS1115(i2c, address=0x49)
+        a1 = AnalogIn(self.ads13, ADS.P0)
+        a2 = AnalogIn(self.ads13, ADS.P1)
+        a3 = AnalogIn(self.ads13, ADS.P2)
+        a4 = AnalogIn(self.adc46, ADS.P0)
+        a5 = AnalogIn(self.adc46, ADS.P1)
+        a6 = AnalogIn(self.adc46, ADS.P2)
 
-while True:
-    a1 = AnalogIn(ads13, ADS.P0)
-    a2 = AnalogIn(ads13, ADS.P1)
-    a3 = AnalogIn(ads13, ADS.P2)
-    a4 = AnalogIn(adc46, ADS.P0)
-    a5 = AnalogIn(adc46, ADS.P1)
-    a6 = AnalogIn(adc46, ADS.P2)
-    print('##########################')
-    print('a1', a1.value, a1.voltage)
-    print('a2', a2.value, a2.voltage)
-    print('a3', a3.value, a3.voltage)
-    print('a4', a4.value, a4.voltage)
-    print('a5', a5.value, a5.voltage)
-    print('a6', a6.value, a6.voltage)
-    sleep(0.5)
+        self.CorNulA1 = a1.value
+        self.CorNulA2 = a2.value
+        self.CorNulA3 = a3.value
+        self.CorNulA4 = a4.value
+        self.CorNulA5 = a5.value
+        self.CorNulA6 = a6.value
+
+    def ReadAmperemeter(self, MassOut: dict):
+        '''
+        Функция опроса датчиков тока 
+        '''
+        a1 = AnalogIn(self.ads13, ADS.P0)
+        a2 = AnalogIn(self.ads13, ADS.P1)
+        a3 = AnalogIn(self.ads13, ADS.P2)
+        a4 = AnalogIn(self.adc46, ADS.P0)
+        a5 = AnalogIn(self.adc46, ADS.P1)
+        a6 = AnalogIn(self.adc46, ADS.P2)
+        # Потенциально кривой матан
+        MassOut['a1'] = round((a1.value - self.CorNulA1) * 0.00057321919, 3)
+        MassOut['a2'] = round((a2.value - self.CorNulA2) * 0.00057321919, 3)
+        MassOut['a3'] = round((a3.value - self.CorNulA3) * 0.00057321919, 3)
+        MassOut['a4'] = round((a4.value - self.CorNulA4) * 0.00057321919, 3)
+        MassOut['a5'] = round((a5.value - self.CorNulA5) * 0.00057321919, 3)
+        MassOut['a6'] = round((a6.value - self.CorNulA6) * 0.00057321919, 3)
+
+        return MassOut
+    
+if __name__ == '__main__':
+    testacp = Acp()
+    while True:
+        mass = testacp.ReadAmperemeter()
+        print(mass['a1'])
+        sleep(0.5)
